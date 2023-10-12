6b94589249c4bf151c342047d603793ab8333a58
romangrbr <romangrbr@gmail.com>
2023-10-12 16:34:08 +0300

    crud base typing

--- app/crud/base.py
+++ app/crud/base.py
@@ -1,15 +1,29 @@
+from typing import Generic, List, Optional, Type, TypeVar
+
+from pydantic import BaseModel
+# Конвертирует в JSON-формат как объекты из базы данных, так и Pydantic-модели
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy import select
+# Класс асинхронной сессии для аннотаций.
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class CRUDBase:
+from app.core.db import Base
+
+ModelType = TypeVar('ModelType', bound=Base)
+CreateSchemaType = TypeVar('CreateSchemaType', bound=BaseModel)
+UpdateSchemaType = TypeVar('UpdateSchemaType', bound=BaseModel)
+
+
+class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
     """Базовый класс CRUD операций"""
 
-    def __init__(self, model):
+    def __init__(self, model: Type[ModelType]):
         self.model = model
 
-    async def get(self, obj_id: int, session: AsyncSession):
+    async def get(
+            self, obj_id: int, session: AsyncSession
+    ) -> Optional[ModelType]:
         """Получить запись по id"""
         # db_obj = await session.execute(self.model, obj_id)
         db_obj = await session.execute(select(self.model).where(
@@ -17,21 +31,29 @@ class CRUDBase:
         ))
         return db_obj.scalars().first()
 
-    async def get_multi(self, session: AsyncSession):
+    async def get_multi(self, session: AsyncSession) -> List[ModelType]:
         """Получить все записи"""
         db_objs = await session.execute(select(self.model))
         return db_objs.scalars().all()
 
-    async def create(self, obj_in, session: AsyncSession):
+    async def create(
+            self, obj_in: CreateSchemaType, session: AsyncSession
+    ) -> ModelType:
         """Создать запись"""
-        obj_in_data = obj_in.dict()
+        obj_in_data = obj_in.dict()  # Конвертируем объект в словарь.
         db_obj = self.model(**obj_in_data)
-        session.add(db_obj)
-        await session.commit()
+        session.add(db_obj)  # Добавляем созданный объект в сессию.
+        await session.commit()  # Записываем изменения непосредственно в БД.
+        # Обновляем объект, считываем данные из БД, чтобы получить его id.
         await session.refresh(db_obj)
         return db_obj
 
-    async def update(self, db_obj, obj_in, session: AsyncSession,):
+    async def update(
+            self,
+            db_obj: ModelType,
+            obj_in: UpdateSchemaType,
+            session: AsyncSession
+    ) -> ModelType:
         """Обновить запись"""
         obj_data = jsonable_encoder(db_obj)  # конвертировать в JSON-формат
         # Перевести в словарь и исключить поля,
@@ -46,15 +68,19 @@ class CRUDBase:
         await session.refresh(db_obj)
         return db_obj
 
-    async def remove(self, db_obj, session: AsyncSession,):
+    async def remove(
+            self, db_obj: ModelType, session: AsyncSession
+    ) -> ModelType:
         """Удалить запись"""
         await session.delete(db_obj)
         await session.commit()
+        # Не обновляем объект через метод refresh(),
+        # следовательно он всё ещё содержит информацию об удаляемом объекте.
         return db_obj
 
     async def get_by_attribute(
             self, attr_name: str, attr_value: str, session: AsyncSession,
-    ):
+    ) -> ModelType:
         """Получить объект по произвольному атрибуту"""
         attr = getattr(self.model, attr_name)
         db_obj = await session.execute(
--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -4,85 +4,111 @@ from sqlalchemy import select
 # Класс асинхронной сессии для аннотаций.
 from sqlalchemy.ext.asyncio import AsyncSession
 # Конвертирует в JSON-формат как объекты из базы данных, так и Pydantic-модели
-from fastapi.encoders import jsonable_encoder
+# from fastapi.encoders import jsonable_encoder
 
+from app.crud.base import CRUDBase
 from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomUpdate
 
 
-async def create_meeting_room(
-        new_room: MeetingRoomCreate,
-        session: AsyncSession
-) -> MeetingRoom:
-    # Конвертируем объект MeetingRoomCreate в словарь.
-    new_room_data = new_room.dict()
-    db_room = MeetingRoom(**new_room_data)
-    # Добавляем созданный объект в сессию.
-    session.add(db_room)
-    # Записываем изменения непосредственно в БД.
-    await session.commit()
-    # Обновляем объект, считываем данные из БД, чтобы получить его id.
-    await session.refresh(db_room)
-    return db_room
+class CRUDMeetingRoom(
+    CRUDBase[
+    MeetingRoom,
+    MeetingRoomCreate,
+    MeetingRoomUpdate
+]):
 
-
-async def get_room_id_by_name(
-        room_name: str,
-        session: AsyncSession
-) -> Optional[int]:
-    """Получить комнату по имени"""
-    # Получаем объект класса Result.
-    db_room_id = await session.execute(
-        select(MeetingRoom.id).where(
-            MeetingRoom.name == room_name
+    async def get_room_id_by_name(
+            self, room_name: str, session: AsyncSession) -> Optional[int]:
+        """Получение объекта по названию"""
+        db_room_id = await session.execute(
+            select(MeetingRoom.id).where(
+                MeetingRoom.name == room_name
+            )
         )
-    )
-    # Извлекаем из него конкретное значение.
-    return db_room_id.scalars().first()
+        db_room_id = db_room_id.scalars().first()
+        return db_room_id
 
 
-async def read_all_rooms_from_db(
-        session: AsyncSession) -> list[MeetingRoom]:
-    """Получить все комнаты из базы"""
-    rooms = await session.execute(select(MeetingRoom))
-    return rooms.scalars().all()
+meeting_room_crud = CRUDMeetingRoom(MeetingRoom)
 
 
-async def get_meeting_room_by_id(
-        room_id: int, session: AsyncSession
-) -> Optional[MeetingRoom]:
-    """Получить комнату по id"""
-    db_room = await session.get(MeetingRoom, room_id)
-    return db_room
+#-------------------------------------------
+# Код ниже заменен базовым классом с аналогичным функционалом
+
+# async def create_meeting_room(
+#         new_room: MeetingRoomCreate,
+#         session: AsyncSession
+# ) -> MeetingRoom:
+#     # Конвертируем объект MeetingRoomCreate в словарь.
+#     new_room_data = new_room.dict()
+#     db_room = MeetingRoom(**new_room_data)
+#     # Добавляем созданный объект в сессию.
+#     session.add(db_room)
+#     # Записываем изменения непосредственно в БД.
+#     await session.commit()
+#     # Обновляем объект, считываем данные из БД, чтобы получить его id.
+#     await session.refresh(db_room)
+#     return db_room
 
 
-async def update_meeting_room(
-        db_room: MeetingRoom,
-        room_in: MeetingRoomUpdate,
-        session: AsyncSession
-) -> MeetingRoom:
-    """Обновить комнату"""
-    # конвертировать в JSON-формат
-    obj_data = jsonable_encoder(db_room)
-    # Перевести в словарь и исключить поля,
-    # которые не были установлены пользователем.
-    update_data = room_in.dict(exclude_unset=True)
-    for field in obj_data:
-        if field in update_data:
-            setattr(db_room, field, update_data[field])
-    session.add(db_room)
-    await session.commit()
-    await session.refresh(db_room)
-    return db_room
+# async def get_room_id_by_name(
+#         room_name: str,
+#         session: AsyncSession
+# ) -> Optional[int]:
+#     """Получить комнату по имени"""
+#     # Получаем объект класса Result.
+#     db_room_id = await session.execute(
+#         select(MeetingRoom.id).where(
+#             MeetingRoom.name == room_name
+#         )
+#     )
+#     # Извлекаем из него конкретное значение.
+#     return db_room_id.scalars().first()
 
 
-async def delete_meeting_room(
-        db_room: MeetingRoom,
-        session: AsyncSession,
-) -> MeetingRoom:
-    # Удалить объект из БД.
-    await session.delete(db_room)
-    await session.commit()
-    # Не обновляем объект через метод refresh(),
-    # следовательно он всё ещё содержит информацию об удаляемом объекте.
-    return db_room
+# async def read_all_rooms_from_db(
+#         session: AsyncSession) -> list[MeetingRoom]:
+#     """Получить все комнаты из базы"""
+#     rooms = await session.execute(select(MeetingRoom))
+#     return rooms.scalars().all()
+
+
+# async def get_meeting_room_by_id(
+#         room_id: int, session: AsyncSession
+# ) -> Optional[MeetingRoom]:
+#     """Получить комнату по id"""
+#     db_room = await session.get(MeetingRoom, room_id)
+#     return db_room
+
+
+# async def update_meeting_room(
+#         db_room: MeetingRoom,
+#         room_in: MeetingRoomUpdate,
+#         session: AsyncSession
+# ) -> MeetingRoom:
+#     """Обновить комнату"""
+#     # конвертировать в JSON-формат
+#     obj_data = jsonable_encoder(db_room)
+#     # Перевести в словарь и исключить поля,
+#     # которые не были установлены пользователем.
+#     update_data = room_in.dict(exclude_unset=True)
+#     for field in obj_data:
+#         if field in update_data:
+#             setattr(db_room, field, update_data[field])
+#     session.add(db_room)
+#     await session.commit()
+#     await session.refresh(db_room)
+#     return db_room
+
+
+# async def delete_meeting_room(
+#         db_room: MeetingRoom,
+#         session: AsyncSession,
+# ) -> MeetingRoom:
+#     # Удалить объект из БД.
+#     await session.delete(db_room)
+#     await session.commit()
+#     # Не обновляем объект через метод refresh(),
+#     # следовательно он всё ещё содержит информацию об удаляемом объекте.
+#     return db_room
