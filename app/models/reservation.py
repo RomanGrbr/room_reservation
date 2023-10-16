ae5ed6074b98e70ebcc3c63914e6f52d0c2ffdf0
romangrbr <romangrbr@gmail.com>
2023-10-16 17:42:44 +0300

    ref

--- app/models/reservation.py
+++ app/models/reservation.py
@@ -7,6 +7,11 @@ class Reservation(Base):
     from_reserve = Column(DateTime)
     to_reserve = Column(DateTime)
     meetingroom_id = Column(Integer, ForeignKey('meetingroom.id'))
+    user_id = Column(Integer, ForeignKey(
+        'user.id',
+        # Имена для внешний ключей, что бы не править миграции
+        #  name='fk_reservation_user_id_user'
+    ))
 
     def __repr__(self):
         return (
